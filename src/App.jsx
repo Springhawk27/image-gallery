0ed5e00436ad3147685fbf6cc721df87128998a1
Springhawk27 <sajjad0mahmud@gmail.com>
2023-11-06 17:51:11 +0600

    setup done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+      <h1 className="text-2xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
